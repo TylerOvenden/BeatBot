98147e42c57cee3b36a39ac5cc38620839f7e61f
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-01-12 11:26:11 -0500

    -

--- BeatPix/src/mainGame/components/Keystroke.java
+++ BeatPix/src/mainGame/components/Keystroke.java
@@ -4,13 +4,17 @@ import java.awt.Graphics2D;
 
 import gui.components.AnimatedComponent;
 
+/*
+ * This class will be mainly coded by Justin Yau
+ * PERIOD 4 & 5 - AP Computer Science Java
+ */
 public class Keystroke extends AnimatedComponent {
 
 	public Keystroke(int x, int y, int w, int h) {
 		super(x, y, w, h);
 		// TODO Auto-generated constructor stub
 	}
-
+	
 	@Override
 	public void update(Graphics2D g) {
 		// TODO Auto-generated method stub
