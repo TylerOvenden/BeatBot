9e660801381426de94387d166ace026976643dab
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-01-12 11:22:19 -0500

    Created Shop Screen

---
+++ BeatPix/src/shop/ShopScreen.java
@@ -0,0 +1,21 @@
+package shop;
+
+import java.util.List;
+
+import gui.interfaces.Visible;
+import gui.userInterfaces.FullFunctionScreen;
+
+public class ShopScreen extends FullFunctionScreen {
+
+	public ShopScreen(int width, int height) 
+	{
+		super(width, height);
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) 
+	{
+		
+	}
+
+}
