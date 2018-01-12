7fe029d9a04fbff6696ee11fe23822bb40dd2fd0
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-12 11:01:47 -0500

    a

---
+++ BeatPix/src/mainGame/MainGUI.java
@@ -0,0 +1,18 @@
+package mainGame;
+
+import gui.GUIApplication;
+
+public class MainGUI extends GUIApplication {
+
+	public MainGUI(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BeatPix/src/screens/GameScreen.java
@@ -0,0 +1,21 @@
+package screens;
+
+import java.util.List;
+
+import gui.interfaces.Visible;
+import gui.userInterfaces.FullFunctionScreen;
+
+public class GameScreen extends FullFunctionScreen {
+
+	public GameScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
