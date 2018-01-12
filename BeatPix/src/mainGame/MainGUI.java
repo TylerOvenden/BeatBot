75b9f9b7959009813a38e9a8aa1e3ebaa7b98fdf
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-12 11:14:13 -0500

    a

--- BeatPix/src/mainGame/MainGUI.java
+++ BeatPix/src/mainGame/MainGUI.java
@@ -5,13 +5,17 @@ import screens.GameScreen;
 
 public class MainGUI extends GUIApplication {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
 	private GameScreen game;
 
 	public static void main(String[] args) {
-		MainGUI game = new MainGUI(1920,1080);
-		Thread runner = new Thread(game);
+		MainGUI s = new MainGUI(1000,1000);
+		Thread runner = new Thread(s);
 		runner.start();
-		
 	}
 	
 	public MainGUI(int width, int height) {
@@ -23,7 +27,6 @@ public class MainGUI extends GUIApplication {
 	public void initScreen() {
 		game = new GameScreen(getWidth(), getHeight());
 		setScreen(game);
-
 	}
 
 }
