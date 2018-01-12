779f2c57a5f16db0b6a604f32f8d08793d15bb37
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2018-01-12 11:39:52 -0500

    finished

--- BeatPix/src/highscore/HighscoreGui.java
+++ BeatPix/src/highscore/HighscoreGui.java
@@ -1,26 +1,31 @@
 package highscore;
 
 import gui.GUIApplication;
-import Highscore.HighscoreGUI;
 
 public class HighscoreGui extends GUIApplication{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private HighscoreScreen highscore;
 	
 	public static void main(String[] args) 
 	{
-		HighscoreGui s = new HighscoreGui(1000,1300);
-		Thread runner = new Thread(s);
+		HighscoreGui g = new HighscoreGui(960,540);
+		Thread runner = new Thread(g);
 		runner.start();
 	}
 	public HighscoreGui(int width, int height) {
 		super(width, height);
+		setVisible(true);
 		
 	}
 
 	@Override
 	public void initScreen() {
-		
+		highscore = new HighscoreScreen(getWidth(), getHeight());
+		setScreen(highscore);
 		
 	}
 
--- BeatPix/src/highscore/HighscoreScreen.java
+++ BeatPix/src/highscore/HighscoreScreen.java
@@ -1,5 +1,21 @@
 package highscore;
 
-public class HighscoreScreen {
+import java.util.List;
+
+import gui.interfaces.Visible;
+import gui.userInterfaces.FullFunctionScreen;
+
+public class HighscoreScreen extends FullFunctionScreen {
+
+	public HighscoreScreen(int width, int height) {
+		super(width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
