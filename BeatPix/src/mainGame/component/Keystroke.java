6d71f96b057dd9506ca95c0167d1362013f3cd8f
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-01-12 11:17:57 -0500

    -

---
+++ BeatPix/src/mainGame/component/Keystroke.java
@@ -0,0 +1,20 @@
+package mainGame.component;
+
+import java.awt.Graphics2D;
+
+import gui.components.Component;
+
+public class Keystroke extends Component {
+
+	public Keystroke(int x, int y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void update(Graphics2D g) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
