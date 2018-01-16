8b5048249a21deade4e5dc39e29c1498ca3b13e0
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-01-16 10:54:58 -0500

    added back button but no action yet

--- BeatPix/src/shop/ShopScreen.java
+++ BeatPix/src/shop/ShopScreen.java
@@ -1,11 +1,21 @@
 package shop;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.util.List;
 
+import gui.components.Action;
+import gui.components.Button;
 import gui.interfaces.Visible;
 import gui.userInterfaces.FullFunctionScreen;
 
 public class ShopScreen extends FullFunctionScreen {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1504601622695326879L;
 
 	public ShopScreen(int width, int height) 
 	{
@@ -14,8 +24,16 @@ public class ShopScreen extends FullFunctionScreen {
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) 
-	{
-		
+	{	
+		Button backButton = new Button(800, 50, 100, 30, "Back", Color.GRAY, new Action() {
+			
+			@Override
+			public void act() {
+				// go back to main screen
+				
+			}
+		});
+		viewObjects.add(backButton);
 	}
 
 }
