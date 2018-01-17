411db4e8193c747acf41574c3293385b01abc57a
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-01-17 10:56:51 -0500

    a

---
+++ BeatPix/src/mainGame/components/Score.java
@@ -0,0 +1,21 @@
+package mainGame.components;
+
+import java.awt.Graphics2D;
+
+import gui.components.Component;
+
+public class Score extends Component{
+	
+	private int score;
+
+	public Score(int x, int y, int w, int h) {
+		super(x, y, w, h);
+	}
+
+	@Override
+	public void update(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
