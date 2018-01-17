d1739e7176d689d527c87fd96eb6e860dfd0a938
Dan <Dan@DELL-INSPIRON.home>
2018-01-16 19:08:12 -0500

    reformatted the layout, added fonts and finished rectangle custom componenet

--- BeatPix/src/shop/CustomRectangle.java
+++ BeatPix/src/shop/CustomRectangle.java
@@ -1,5 +1,6 @@
 package shop;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
@@ -18,9 +19,13 @@ public class CustomRectangle extends Component
 	@Override
 	public void update(Graphics2D g) 
 	{
+
+		int thickness = 10;
+		g.setStroke(new BasicStroke(thickness));
 		g.setColor(color);
 		g.drawRect(0, 0, getWidth(), getHeight());
-		g.fillRect(0, 0, getWidth(), getHeight());
+		g.setStroke(g.getStroke());
+		
 		
 	}
 
--- BeatPix/src/shop/ShopScreen.java
+++ BeatPix/src/shop/ShopScreen.java
@@ -1,6 +1,8 @@
 package shop;
 
 import java.awt.Color;
+import java.awt.Font;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,9 +14,11 @@ public class ShopScreen extends FullFunctionScreen
 	private Graphic background;	
 	private TextLabel currency;
 	private CustomRectangle songArea;
-
-
-
+	private TextLabel banner;
+	private TextLabel credit;
+	private Font bannerFont;
+	private Font creditFont;
+	private ArrayList songs;
 	public ShopScreen(int width, int height) 
 	{
 		super(width, height);
@@ -23,23 +27,31 @@ public class ShopScreen extends FullFunctionScreen
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) 
 	{
+	//	Font.createFont(1, "resources//slkscr.ttf");
+		bannerFont = new Font("Verdana", Font.ITALIC, 25);
+		creditFont = new Font("Verdana", Font.BOLD, 20);
 		background = new Graphic(0,0,getWidth(),getHeight(),"resources//sample_bg.gif");
 		viewObjects.add(background);
 		
 		//background for credits, make new component later
-		CustomRectangle creditBG = new CustomRectangle(185,60,90,33, Color.white);
+		CustomRectangle creditBG = new CustomRectangle(135,60,190,40, Color.white);
 		viewObjects.add(creditBG);
 		
 		String credits = "Credits: "; // add method "getCredits()" later
-		currency = new TextLabel(200,60,200,200, credits);
-		viewObjects.add(currency);
+		credit = new TextLabel(150,60,200,200, credits);
+		credit.setCustomTextColor(Color.white);
+		credit.setFont(creditFont);
+		viewObjects.add(credit);
 		
 		// where the new songs will be placed
-		songArea = new CustomRectangle(130, 150, 225, 350, Color.white);
+		songArea = new CustomRectangle(130, 150, 225, 350, Color.gray);
 		viewObjects.add(songArea);
 		
-		//area where "SONGS" will be displayed, new component
-	//	songBanner = new TextLabel(130, 120, 200,200, "Songs");
+		//area where "SONGS" will be displayed
+		banner = new TextLabel(160, 100,200,200, "Songs");
+		banner.setCustomTextColor(Color.white);
+		banner.setFont(bannerFont);
+		viewObjects.add(banner);
 	//	ScrollablePane a = new ScrollablePane(null, null, 50, 50, 20, 30);
 	//	viewObjects.add(a);
 	}	
