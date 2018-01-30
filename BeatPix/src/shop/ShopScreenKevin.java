906f289b367a9ea98e448624b538117f7b8dfb60
TrishulaFaeris <klin4640@bths.edu>
2018-01-29 22:40:04 -0500

    got some things down

--- BeatPix/src/shop/ShopScreenKevin.java
+++ BeatPix/src/shop/ShopScreenKevin.java
@@ -1,6 +1,7 @@
 package shop;
 
 import java.awt.Color;
+import java.awt.Rectangle;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,16 +23,19 @@ public class ShopScreenKevin extends FullFunctionScreen {
 	private static final long serialVersionUID = 1504601622695326879L;
 	
 	private int counter;
-	private ArrayList<Button> buttons;
+	private ArrayList<TextLabel> text;
+	CustomRectangle border;
+	ScrollablePane charScroll;
+
 	public ShopScreenKevin(int width, int height) 
 	{
 		super(width, height);
-		
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) 
 	{	
+		text = new ArrayList<TextLabel>();
 		Button backButton = new Button(800, 50, 100, 30, "Back", Color.GRAY, new Action() {
 			
 			@Override
@@ -40,24 +44,34 @@ public class ShopScreenKevin extends FullFunctionScreen {
 				
 			}
 		});
-		ScrollablePane charScroll = new gui.components.ScrollablePane(this, 100, 100, 100, 100);
+		charScroll = new ScrollablePane(this, 650, 100, 250, 400);
 		charScroll.setBorderWidth(3);
-		for(int i=0; i < 10; i++){
-			//name change 
+		//change the 10, to the number of costumes, 10 is just a place holder
+		for(int i=0; i < 2; i++){ 
+
 			charScroll.addObject(new Button(5,30*i,100,25,"Button "+(i+1), new Action() {
 				
 				@Override
 				public void act() {
-					// TODO Auto-generated method stub
-					
+					border.setVisible(true);
+					text.get(0).setVisible(true);
 				}
 			}));
 		}
-		charScroll.update();
+		
+		 border = new CustomRectangle(280, 180, 220, 120, Color.BLACK, 3);
+		 text.add(new TextLabel(300, 200, 200, 100, "Do you wish to unlock this0?"));
+		 text.add(new TextLabel(300, 200, 200, 100, "Do you wish to unlock this1?"));
+		 border.setVisible(false);
+		 text.get(0).setVisible(false);
+		 text.get(1).setVisible(false);
+		
 		
 		viewObjects.add(backButton);
+		viewObjects.add(border);
+		viewObjects.add(text.get(0));
+		viewObjects.add(text.get(1));
 		viewObjects.add(charScroll);
+		
 	}
-
-
 }
