2012ad91fa215a75a7ac823d5c52356c8cfe125d
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-02-05 11:15:52 -0500

    visual aspect working of leveselectG

--- BeatPix/src/screens/LevelSelectG.java
+++ BeatPix/src/screens/LevelSelectG.java
@@ -31,24 +31,32 @@ public class LevelSelectG extends FullFunctionScreen{
 	public void initAllObjects(List<Visible> viewObjects) {
 		background = updateBackground("resources\\mop.png");
 		viewObjects.add(background);
+		unseenButtons= new ArrayList<ImageButton>();
 		ImageIcon icon = new ImageIcon("resources\\tester.jpg");
 		buttons = new ArrayList<ImageButton>();
 		for(int i=0; i<5; i++) {
 /*P D*/		buttons.add(new ImageButton( 180*(-i-1)+getWidth()-10, 80*(i+1) + getHeight()-580, icon.getIconWidth(), 100 ,"resources\\tester.jpg"));
 				
-		buttons.get(i).setEnabled(true);
+		
 		
 		}
 		buttons.get(2).loadImages("resources\\tester1.jpg", buttons.get(2).getWidth()+25, buttons.get(2).getHeight()+25);
-		unseenButtons.add(null);
+		buttons.get(2).setAction(new Action() {
+			public void act(){
+				Test.test.setScreen(new LevelSelectG(getWidth(),getHeight()));
+			}
+	});
 		
 		
 		ImageButton left = new ImageButton( getWidth()-900, getHeight()-300, icon.getIconWidth(), 100 ,"resources\\LeftArrow-small.jpg");
 		viewObjects.add(left);
+		left.setEnabled(true);
+
 		left.setAction(new Action() {
 			public void act(){
+				System.out.println("asd");
 				unseenButtons.add(buttons.get(4));
-
+				System.out.println("asd");
 				buttons.set(4, buttons.get(3));
 				buttons.set(3, buttons.get(2));
 				buttons.set(2, buttons.get(1));
@@ -59,17 +67,44 @@ public class LevelSelectG extends FullFunctionScreen{
 					buttons.get(i).setX(180*(-i-1)+getWidth()-10);
 					buttons.get(i).setY(80*(i+1) + getHeight()-580);
 				}
+				buttons.get(2).setEnabled(true);
 				buttons.get(2).loadImages("resources\\tester1.jpg", buttons.get(2).getWidth()+25, buttons.get(2).getHeight()+25);
-				buttons.get(2).setAction(new Action() {
-					public void act(){
-
-					}
-			});
+				
+				buttons.get(3).setEnabled(false);
+				buttons.get(3).loadImages("resources\\tester2.jpg", buttons.get(3).getWidth()-25, buttons.get(3).getHeight()-25);
+				
 			}
+			
 	});
 		
 		ImageButton right = new ImageButton( getWidth()-200, getHeight()-300, icon.getIconWidth(), 100 ,"resources\\rightArrow-small.jpg");
+		right.setEnabled(true);
 		viewObjects.add(right);
+		
+		right.setAction(new Action() {
+			public void act(){
+				System.out.println("asd");
+				unseenButtons.add(buttons.get(0));
+				System.out.println("asd");
+				buttons.set(0, buttons.get(1));
+				buttons.set(1, buttons.get(2));
+				buttons.set(2, buttons.get(3));
+				buttons.set(3, buttons.get(4));
+				buttons.set(4, unseenButtons.get(unseenButtons.size()-1));
+				unseenButtons.remove(unseenButtons.size()-1);
+				for(int i=0; i<5; i++) {
+					buttons.get(i).setX(180*(-i-1)+getWidth()-10);
+					buttons.get(i).setY(80*(i+1) + getHeight()-580);
+				}
+				buttons.get(2).setEnabled(true);
+				buttons.get(2).loadImages("resources\\tester1.jpg", buttons.get(2).getWidth()+25, buttons.get(2).getHeight()+25);
+				
+				buttons.get(1).setEnabled(false);
+				buttons.get(1).loadImages("resources\\tester2.jpg", buttons.get(1).getWidth()-25, buttons.get(1).getHeight()-25);
+				
+			}
+			
+	});
 		
 		for(int i=0; i<5; i++) {
 			viewObjects.add(buttons.get(i));
