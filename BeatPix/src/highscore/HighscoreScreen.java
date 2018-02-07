2459efc29bacaa28f82f55408b6346106b9788a2
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-07 11:23:51 -0500

    a

--- BeatPix/src/highscore/HighscoreScreen.java
+++ BeatPix/src/highscore/HighscoreScreen.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import gui.interfaces.Visible;
 import gui.userInterfaces.FullFunctionScreen;
+import mainGame.components.CustomText;
 import mainGame.components.Song;
 
 public class HighscoreScreen extends FullFunctionScreen {
@@ -13,17 +14,30 @@ public class HighscoreScreen extends FullFunctionScreen {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private boolean isWin;
+	private int score;
+	private double acc;
 	public ArrayList<Song> songList;
+	public CustomText condition;
+	public CustomText currentScore;
+	public CustomText currentAcc;
 
-	public HighscoreScreen(int width, int height) {
+	public HighscoreScreen(int width, int height,boolean isWin,int score,double acc) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		this.isWin=isWin;
+		this.score=score;
+		this.acc=acc;
 	}
 
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
-		// TODO Auto-generated method stub
-		
+		condition=new CustomText(400, 100, 100, 100, "You Lose");
+		viewObjects.add(condition);
+		if(!isWin) {
+			condition.setText("You Win");
+		}
+		currentScore=new CustomText(400, 200, 100, 100, "Score: "+score);
+		viewObjects.add(currentScore);
 	}
 
 }
--- BeatPix/src/mainGame/components/interfaces/Highscore.java
+++ BeatPix/src/mainGame/components/interfaces/Highscore.java
@@ -2,4 +2,5 @@ package mainGame.components.interfaces;
 
 public interface Highscore {
 	public int[] getScores();
+	public float[] getAcc();
 }
