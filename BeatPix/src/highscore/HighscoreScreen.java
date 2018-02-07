49a1b9c260eb5d74203df8cfcbd0d392d85a1451
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-07 11:08:46 -0500

    a

--- BeatPix/src/highscore/HighscoreScreen.java
+++ BeatPix/src/highscore/HighscoreScreen.java
@@ -1,11 +1,19 @@
 package highscore;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import gui.interfaces.Visible;
 import gui.userInterfaces.FullFunctionScreen;
+import mainGame.components.Song;
 
 public class HighscoreScreen extends FullFunctionScreen {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public ArrayList<Song> songList;
 
 	public HighscoreScreen(int width, int height) {
 		super(width, height);
---
+++ BeatPix/src/mainGame/components/interfaces/Highscore.java
@@ -0,0 +1,5 @@
+package mainGame.components.interfaces;
+
+public interface Highscore {
+	public int[] getScores();
+}
