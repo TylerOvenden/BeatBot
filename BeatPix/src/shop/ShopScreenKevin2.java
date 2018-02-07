65588ec0d5420bfccbf5aab78d1e2fce825b7fb3
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2018-02-07 11:31:14 -0500

    okay added images

--- BeatPix/src/shop/ShopScreenKevin2.java
+++ BeatPix/src/shop/ShopScreenKevin2.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import gui.components.Action;
+import gui.components.AnimatedComponent;
 import gui.components.Button;
 import gui.components.Component;
 import gui.components.FullFunctionPane;
@@ -16,9 +17,10 @@ public class ShopScreenKevin2 extends FullFunctionScreen {
 	private ArrayList<Button> buttonList;
 	private int index;
 	private ArrayList<Integer> indexList;
+	private String[] imageName;
+	private ArrayList<ImageButton> images;
 	public ShopScreenKevin2(int width, int height) {
 		super(width, height);
-
 	}
 
 	@Override
@@ -26,8 +28,13 @@ public class ShopScreenKevin2 extends FullFunctionScreen {
 		// TODO Auto-generated method stub
 		buttonList = new ArrayList<Button>();
 		indexList = new ArrayList<Integer>();
+		images = new ArrayList<ImageButton>();
+		imageName = new String[] {"resources/redGuy.png", "resources/greenGuy.png", "resources/whiteGuy.png"};
+		for(int i = 0; i < imageName.length; i ++) {
+			images.add(new ImageButton(100, (100*i)+50, 200, 300, imageName[i], null, null));
+		}
 		createIntList(5);
-		for(int i=0; i < 5; i++){ 
+		for(int i=0; i < 3; i++){ 
 			//got the index number
 			final int x = i;
 			buttonList.add(new Button(5,(50*i)+50,100,25,"Button "+i, new Action() {
@@ -40,14 +47,21 @@ public class ShopScreenKevin2 extends FullFunctionScreen {
 				System.out.println("The index number is "+index);			
 				buttonList.get(j).setVisible(false);
 				indexList.remove(index);
+				images.get(j).setVisible(true);
 				
 
 				}
 			}));
 		}
+		
 		for(int a = 0; a < buttonList.size(); a++) {
+			images.get(a).setVisible(false);
 			viewObjects.add(buttonList.get(a));
+			viewObjects.add(images.get(a));
+			
 		}
+
+		
 	}
 	public void createIntList(int a) {
 		for(int i = 0; i < a; i++) {
