89e05a8e66aba01c08b3064775dffeb8b0a7b614
Tyler2017 <Tyler2017@LAPTOP-MELKJ2M8>
2018-02-08 18:06:21 -0500

    d

--- BeatPix/src/mainGame/components/PlaySong.java
+++ BeatPix/src/mainGame/components/PlaySong.java
@@ -11,89 +11,38 @@ import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.SourceDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
 import mainGame.components.interfaces.JustinPlaySongInterface;
 import mainGame.screens.GameScreen;
  
 
-public class PlaySong implements JustinPlaySongInterface {
- 
-    // size of the byte buffer used to read/write the audio stream
-    private static final int BUFFER_SIZE = 4000;
-     
-    /**
-     * Play a given audio file.
-     * @param audioFilePath Path of the audio file.
-     * Tyler
-     */
-    public void play(String audioFilePath) {
-        File audioFile = new File(audioFilePath);
-        try {
-            AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
- 
-            AudioFormat format = audioStream.getFormat();
- 
-            DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
- 
-            SourceDataLine audioLine = (SourceDataLine) AudioSystem.getLine(info);
- 
-            audioLine.open(format);
- 
-            audioLine.start();
-             
-           // System.out.println("Playback started.");
-             
-            byte[] bytesBuffer = new byte[BUFFER_SIZE];
-            int bytesRead = -1;
-            try {
-				Thread.sleep(GameScreen.game.calculateTotalFallTime());
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-            while ((bytesRead = audioStream.read(bytesBuffer)) != -1) {
-                audioLine.write(bytesBuffer, 0, bytesRead);
-            }
-             
-            audioLine.drain();
-            audioLine.close();
-            audioStream.close();
-             
-           // System.out.println("Playback completed.");
-             
-        } catch (UnsupportedAudioFileException ex) {
-            System.out.println("The specified audio file is not supported.");
-            ex.printStackTrace();
-        } catch (LineUnavailableException ex) {
-            System.out.println("Audio line for playing back is unavailable.");
-            ex.printStackTrace();
-        } catch (IOException ex) {
-            System.out.println("Error playing the audio file.");
-            ex.printStackTrace();
-        }      
+public class PlaySong extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Media pick = new Media("resources/audio.mp3"); // replace this with your own audio file
+        MediaPlayer player = new MediaPlayer(pick);
+
+        // Add a mediaView, to display the media. Its necessary !
+        // This mediaView is added to a Pane
+        MediaView mediaView = new MediaView(player);
+
+
+
+        // Play the media once the stage is shown
+        player.play();
     }
-     
+
     public static void main(String[] args) {
-        String audioFilePath = "resources/maps/DreadnoughtMastermind(xi+nora2r)/DreadnoughtMastermind(xi+nora2r).wav";
-        PlaySong player = new PlaySong();
-        player.play(audioFilePath);
+         launch(args);
     }
 
-	@Override
-	public void pauseSong() {
-		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void resumeSong() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void stopSong() {
-		// TODO Auto-generated method stub
-		
-	}
- 
 }
