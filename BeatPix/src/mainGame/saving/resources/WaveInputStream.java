695d80bb29e64e755b41c40c90254ff7ea25bbbb
Justin Yau <justiceman8998@gmail.com>
2018-02-09 22:33:10 -0500

    -

--- BeatPix/src/mainGame/saving/resources/WaveInputStream.java
+++ BeatPix/src/mainGame/saving/resources/WaveInputStream.java
@@ -1,11 +1,20 @@
 package mainGame.saving.resources;
 
+import java.io.BufferedInputStream;
 import java.io.InputStream;
 
 public class WaveInputStream {
 
-	public WaveInputStream( InputStream in) {
-		// TODO Auto-generated constructor stub
+	private final EndianInputStream in;
+	
+	public WaveInputStream(InputStream stream) {
+		
+		if(stream == null ) {
+			throw new IllegalArgumentException("Input stream cannot be null!");
+		}
+		
+		in = new EndianInputStream(new BufferedInputStream(stream, 1024*1024));
+
 	}
 
 }
