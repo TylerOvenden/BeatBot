954346afad1928971dda3a8fb2a7f3839517a77a
Justin Yau <justiceman8998@gmail.com>
2018-02-09 21:53:39 -0500

    Added resources

--- BeatPix/src/mainGame/saving/MusicFileConverter.java
+++ BeatPix/src/mainGame/saving/MusicFileConverter.java
@@ -1,6 +1,7 @@
 package mainGame.saving;
 
 import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,6 +13,8 @@ import java.util.ArrayList;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.TargetDataLine;
 import javax.sound.sampled.UnsupportedAudioFileException;
 
 /**
@@ -23,12 +26,7 @@ public class MusicFileConverter {
 
 	//Algorithm is derived by http://archive.gamedev.net/archive/reference/programming/features/beatdetection/index.html
 	/*
-	 * Instant Energy Formula: 
-	 * 
-	 * Local Average Energy Formula:
-	 * 44032 - About one second - Call the .getFrameRate() to be 100 percent sure
-	 * 
-	 * (New Samples/Samples That System Remember) SUMMATION FROM 0 TO NEW SAMPLES 
+	 * 44032 - About one second - Call the .getFrameRate() to get exact rate per second to be sure
 	 * 
 	 */
 	
@@ -38,13 +36,19 @@ public class MusicFileConverter {
 	private int[] previousData;
 	private long framesCount;
 	private AudioInputStream audioInputStream;
+	private FileInputStream fileInputStream;
+	private DataInputStream dataInputStream;
 	private AudioFormat audioFormat;
+	private ArrayList<float[]> samples;
 	private ArrayList<int[]> beats;
 	
 	public MusicFileConverter() {
 		File file = new File("resources/maps/adrenaline!!! -TV Ver-TrySail/adrenaline!!! -TV Ver-TrySail.wav");
 		try {
 			audioInputStream = AudioSystem.getAudioInputStream(new BufferedInputStream(new FileInputStream(file)));
+			fileInputStream = new FileInputStream(file);
+			dataInputStream = new DataInputStream(fileInputStream);
+			
 			audioFormat = audioInputStream.getFormat();
 			sampleSize = audioFormat.getSampleSizeInBits()/8;
 		    channelsNum = audioFormat.getChannels();
@@ -54,10 +58,10 @@ public class MusicFileConverter {
 	        long dataLength = framesCount * audioFormat.getSampleSizeInBits() * audioFormat.getChannels() / 8;
 
 	        data = new byte[(int) dataLength];
-	        audioInputStream.read(data);
-	        System.out.println(audioFormat.getFrameRate());
+	        //System.out.println(audioFormat.getFrameRate());
 	        
-	        addBeats();
+	        System.out.println(getSampleNumber(132000));
+	        //addBeats();
 	        
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
@@ -80,28 +84,66 @@ public class MusicFileConverter {
 	public void addBeats() {
 		beats = new ArrayList<int[]>();
 		previousData = new int[2];
-        for (int i = 0; i < framesCount; i++) {
-        	float d = getSampleFloat(i);
+        for (int i = 1024; i < framesCount - 1024; i++) {
+        	if(determineInstantEnergy(i) >= (determineAverageLocalEnergy(i) * 1.3)) {
+        		System.out.println(true);
+        	}
+        	//float d = getSampleFloat(i);
         	//int time = (int) ((i / audioFormat.getFrameRate()) * 1000); //Divides the current frame by the frame rate to get the time in seconds and then multiply by 1000 to convert to milliseconds
         }
+	}
+	
+	public float determineAverageLocalEnergy(int start) {
+		float sum = 0;
+		for(int i = 0; i < 43; i++) {
+			sum += Math.pow(determineInstantEnergy(start - i), 2);
+		}
+		return sum / 43;
+	}
+	
+	public float determineInstantEnergy(int start) {
+		float value = 0;
+		for(int i = start; i < 1024 + start; i++) {
+			//value += Math.pow(getSampleNumber(i)[0], 2) + Math.pow(getSampleNumber(i)[1], 2);
+		}
+		return value;
 	}
 	
 	public int getRandomNumber(int low, int high) {
 		return low + (int)(Math.random() * ((high - low) + 1));
 	}
 	
-    public float getSampleFloat(int sampleNumber) {
+    public void getSamples() {
 
+    	samples = new ArrayList<float[]>();
+    	
+    	for(int i = 0; i < framesCount; i++) {
+    		short leftChannel;
+			try {
+				leftChannel = Short.reverseBytes(dataInputStream.readShort());
+	    		short rightChannel = Short.reverseBytes(dataInputStream.readShort());
+	    		float leftC = leftChannel;
+	    		float rightC = rightChannel;
+	    		float[] temp = {leftC, rightC};
+	    		samples.add(temp);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+    	}
+    	
+    }
+    
+    public int getSampleNumber(int sampleNumber) {
         if (sampleNumber < 0 || sampleNumber >= data.length / sampleSize) {
-            throw new IllegalArgumentException("WARNING: sample number can't be < 0 or >= data.length/" + sampleSize);
+            throw new IllegalArgumentException("Warning! Sample number can't be < 0 or >= data.length/" + sampleSize);
         }
 
-        byte[] sampleBytes = new byte[4]; 
-        
+        byte[] sampleBytes = new byte[4];
+
         for (int i = 0; i < sampleSize; i++) {
             sampleBytes[i] = data[sampleNumber * sampleSize * channelsNum + i];
         }
-
+        
         int sample = ByteBuffer.wrap(sampleBytes).order(ByteOrder.LITTLE_ENDIAN).getInt();
         return sample;
     }
