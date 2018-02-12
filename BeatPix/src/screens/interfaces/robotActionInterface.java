007ba74af4ae736beb7123675fc2d90ca8a87681
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2018-02-12 11:13:33 -0500

    ONE METHOD INTERFACE

--- BeatPix/src/screens/interfaces/robotActionInterface.java
+++ BeatPix/src/screens/interfaces/robotActionInterface.java
@@ -3,8 +3,5 @@ package screens.interfaces;
 public class robotActionInterface {
 
 	boolean isHit; //GIVE ME THE MISSES MANG (getter for if the player misses the arrow)
-	
-	String keyThatIsPressed; //GVE ME THE KEYS MANG (getter for keys)
-	
 }
 
