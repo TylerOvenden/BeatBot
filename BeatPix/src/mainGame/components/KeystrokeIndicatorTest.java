dd93221cd51d6676404dc4da026272fa30b5d8cb
BT_1N3_09 <BT_1N3_09@BT_1N3_09.bktech.local>
2018-02-14 11:18:29 -0500

    a

---
+++ BeatPix/src/mainGame/components/KeystrokeIndicatorTest.java
@@ -0,0 +1,40 @@
+package mainGame.components;
+
+import java.awt.Graphics2D;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.KeyStroke;
+
+import gui.components.Component;
+
+public class KeystrokeIndicatorTest extends Component {
+	
+	private String path;
+	private boolean isHit;
+	
+
+	public KeystrokeIndicatorTest(int x, int y,int w,int h, String p) {
+		super(x, y, w, h);
+		this.path=p;
+		isHit=false;
+	}
+
+	@Override
+	public void update(Graphics2D g) {
+		super.clear();
+		ImageIcon icon;
+		icon = new ImageIcon(path);
+		Image img = icon.getImage();
+		Image newimg;
+		if(isHit) {
+			newimg = img.getScaledInstance(50,50,java.awt.Image.SCALE_SMOOTH);
+			
+		}else {
+			newimg=img;
+		}
+		ImageIcon newIcon = new ImageIcon(newimg);
+		g.drawImage(newIcon.getImage(), 0, 0, null);	
+	}
+
+}
