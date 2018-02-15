8b0bfdaf9c5b444654d2878da7b141813cc6ac2b
TrishulaFaeris <klin4640@bths.edu>
2018-02-14 19:49:13 -0500

    wee, my stuff disappeared again wee

--- BeatPix/src/shop/CharacterSelectionScreen.java
+++ BeatPix/src/shop/CharacterSelectionScreen.java
@@ -13,103 +13,146 @@ public class CharacterSelectionScreen extends FullFunctionScreen implements unlo
 	/**
 	 * 
 	 */
+	//Kevin class + kevin interface
 	private static final long serialVersionUID = 1L;
 	private ArrayList<ImageButton> imagesButton;
 	private ArrayList<ImageButton> selectImage;
 	private ArrayList<Button> buttons;
+	
 	private int numChars;
+	
 	private String[] imageNames;
 	private Boolean[] unlock;
 	private String[] colorArray;
-	private String  color;
-	private TextLabel notUnlocked;
-	private Button okay;
-	private Graphic backGround;
+	
 	private CustomText back;
+	private CustomText lockedText;
+	private CustomText selectText;
+	
+	private String  color;
+	private Graphic backGround;
+	private Graphic border;
 	private Button backButton;
+	
 	public CharacterSelectionScreen(int width, int height) {
 		// TODO Auto-generated constructor stub
 		super(width, height);
 	}
+	
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 		numChars = 4;
 		imagesButton = new ArrayList<ImageButton>();
 		selectImage = new ArrayList<ImageButton>();
 		buttons = new ArrayList<Button>();
-		notUnlocked = new TextLabel(400, 200, 100, 100, "Not Unlocked");
-		notUnlocked.setCustomTextColor(Color.WHITE);
+		
 		imageNames = new String[] {"resources/sprites/defaultGuy.png","resources/sprites/redGuy.png", "resources/sprites/greenGuy.png", "resources/sprites/whiteGuy.png"};
 		unlock = new Boolean[] {true, false, false, false};
 		colorArray = new String[] {"default", "red", "green" , "white"};
+		
 		color = colorArray[0];
+		
 		backGround = new Graphic(0, 0, getWidth(), getHeight(), "resources/charaBG.png");
-		okay = new Button(400, 130, 100, 30, "okay", Color.GRAY, new Action() {
-			
-			@Override
-			public void act() {
-				//able all the buttons
-				notUnlocked.setVisible(false);
-				okay.setVisible(false);
-				
-			}
-		});
+		border = new Graphic(212, 180, 305,305,"resources//shop//TransparentButtonB.png");
+		
+		lockedText = new CustomText(265, 210, 200, 370, "Locked",false);
+		selectText = new CustomText(265, 210, 200, 370, "Selected",true);
+		
 		for(int i = 0; i < imageNames.length; i ++) {
 			final int x = i;	
 			selectImage.add(new ImageButton(700, 180, 200, 300, imageNames[i], ""));
 			imagesButton.add(new ImageButton((125*i)+125, 50, 100, 300, imageNames[i], ""));
+			
 			buttons.add(new Button((125*i)+125, 50, 100, 300, "", new Action() {
 				
 				@Override
 				public void act() {
 					int j = x;
-					//disable all the buttons
 					if(unlock[j] == true) {
-						for(int z = 0; z < numChars; z++) {
-							selectImage.get(z).setVisible(false);
-						}
-						selectImage.get(j).setVisible(true);
-						color = colorArray[j];
+						new Thread() {
+							public void run() {
+								try {
+									notDisableBut(false);
+									color = colorArray[j];
+									for(int z = 0; z < numChars; z++) {
+										selectImage.get(z).setVisible(false);
+									}
+									selectImage.get(j).setVisible(true);
+									displayText(selectText, true);
+									
+									Thread.sleep(750);
+									
+									displayText(selectText, false);
+									notDisableBut(true);
+								}catch(InterruptedException e){
+									
+								}
+							}
+						} .start();
 					}else {
-						notUnlocked.setVisible(true);
-						okay.setVisible(true);
-						
+						new Thread() {
+							public void run() {
+								try {
+									notDisableBut(false);
+									displayText(lockedText, true);
+									
+									Thread.sleep(750);
+									
+									displayText(lockedText, false);
+									notDisableBut(true);
+									
+								}catch(InterruptedException e){
+									
+								}
+							}
+						}.start();
 					}
 				}
 			}));
 		}
 		 back = new CustomText(805, 60, 95, 50, "Back", true);
 		 backButton = new Button(800, 50, 100, 30, "", Color.GRAY, new Action() {
-			
 			@Override
 			public void act() {
 			MainGUI.test.setScreen(MainGUI.mainMenu);
 			MainGUI.mainMenu.changeIdle();
 			}
 		});
+		 
 		//set things to visible 
 		for(int z = 0; z < numChars; z++) {
 			selectImage.get(z).setVisible(false);
 		}
 		selectImage.get(0).setVisible(true);
 		backGround.setVisible(true);
-		notUnlocked.setVisible(false);
-		okay.setVisible(false);
+		border.setVisible(false);
+		lockedText.setVisible(false);
+		selectText.setVisible(false);
 		
 		//viewobjects
 		viewObjects.add(backGround);
 		viewObjects.add(backButton);
+		viewObjects.add(border);
+		viewObjects.add(lockedText);
+		viewObjects.add(selectText);
 		for(int k = 0; k < numChars; k++) {
 			viewObjects.add(buttons.get(k));
 			viewObjects.add(imagesButton.get(k));
 			viewObjects.add(selectImage.get(k));
 		}
-		viewObjects.add(okay);
-		viewObjects.add(notUnlocked);
 		viewObjects.add(back);
 	}
-
-
+	//helper methods
+	public void notDisableBut(boolean b) {
+		for(int i  = 0; i < numChars; i++) {
+			buttons.get(i).setEnabled(b);
+		}
+	}
+	public void displayText(CustomText a, boolean b) {
+		border.setVisible(b);
+		a.setVisible(b);
+	}
+	//interfaceMethods
 	@Override
 	public void unlock(int i) {
 		unlock[i+1] = true;
