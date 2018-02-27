be84d7169157c3903860f6719c501dc27121a100
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-02-27 11:27:39 -0500

    mediaplayer

---
+++ BeatPix/src/javaFX/MainController.java
@@ -0,0 +1,33 @@
+package javaFX;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.fxml.Initializable;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+public class MainController implements Initializable {
+	private MediaView mv;
+	private MediaPlayer mp;
+	private Media me;
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		String path = new File("resources/music/keroacoustic.wav").getAbsolutePath();
+		me = new Media(new File(path).toURI().toString());
+		mp = new MediaPlayer(me);
+		mv.setMediaPlayer(mp);
+		mp.setAutoPlay(true);
+		DoubleProperty width = mv.fitWidthProperty();
+		DoubleProperty height = mv.fitHeightProperty();
+		width.bind(Bindings.selectDouble(mv.sceneProperty(), "width"));
+		height.bind(Bindings.selectDouble(mv.sceneProperty(), "height"));
+	}
+
+}
