459bd43d32068d41394df22cb09f2e43bdd83eda
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-02-28 11:18:29 -0500

    alerts

---
+++ BeatPix/src/javaFX/Main1.java
@@ -0,0 +1,37 @@
+package javaFX;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class Main1 extends Application{
+
+	Stage window;
+	Button button;
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		window = primaryStage;
+		window.setTitle("title");
+		
+		button = new Button("click");
+		button.setOnAction(e -> AlertBox.display("jeff", "clicked"));
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		Scene scene = new Scene(layout,300,350);
+		window.setScene(scene);
+		window.show();
+		
+		
+	}
+
+}
